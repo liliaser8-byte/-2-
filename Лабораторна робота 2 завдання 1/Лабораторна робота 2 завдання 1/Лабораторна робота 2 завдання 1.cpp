b5bcc83b8cd234cde02baa98622da1c11cb616c5
liliaser8-byte <liliaser8@gmail.com>
2025-11-25 13:30:32 +0200

    Add files via upload

---
+++ Лабораторна робота 2 завдання 1/Лабораторна робота 2 завдання 1/Лабораторна робота 2 завдання 1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath> 
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double x, y;
+
+    std::cout << "Введіть значення x: ";
+    std::cin >> x;
+
+    if (x < -1) {
+        y = pow(sin(x), 3) * pow(cos(x), 2);
+    }
+    else {
+        y = log(abs(x) + 2.0) * exp(x + 1.0);
+    }
+
+    std::cout << "Значення y = " << y << std::endl;
+
+    return 0;
+}
---
+++ Лабораторна робота 2 завдання 2/Лабораторна робота 2 завдання 2/Лабораторна робота 2 завдання 2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+#include <windows.h> 
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int trainNumber;
+
+    cout << "Введіть номер поїзда: ";
+    cin >> trainNumber;
+
+    cout << "Пункт призначення: ";
+    switch (trainNumber) {
+    case 1:
+        cout << "Київ" << endl;
+        break;
+    case 2:
+        cout << "Львів" << endl;
+        break;
+    case 3:
+        cout << "Одеса" << endl;
+        break;
+    case 4:
+        cout << "Харків" << endl;
+        break;
+    case 5:
+        cout << "Дніпро" << endl;
+        break;
+    default:
+        cout << "Невідомий номер поїзда" << endl;
+        break;
+    }
+
+    return 0;
+}
